e566d528376a22af7fee15f6e16ba9d540a427a5
Abdürrahim YILMAZ <mail@ayilmaz.xyz>
2024-08-24 15:40:56 +0300

    feat: CLI
    Dead simple cli

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from jinja2 import Environment, FileSystemLoader
 from yaml import safe_load
+import argparse
+
+# Parse the command line arguments
+parser = argparse.ArgumentParser(description='Render a Jinja2 template with a YAML config')
+parser.add_argument('config', help='Path to the YAML config file')
+parser.add_argument('-t', '--template', help='Path to the Jinja2 template file (if not set interactively selects)', default=None, nargs='?')
+parser.add_argument('-o', '--output', help='Path to the output file (if not set prints the result)', default=None, nargs='?')
+args = parser.parse_args()
 
 # Load and parse the YAML config
-config = safe_load(open('linkjinja.yaml'))
+config = safe_load(open(args.config))
 
-# Set up the Jinja2 environment and load the template
-file_loader = FileSystemLoader('templates')  # Directory where template.html is located
-env = Environment(loader=file_loader)
-template = env.get_or_select_template('template.jinja')
+if args.template:
+    env = Environment(loader=FileSystemLoader('.'))
+    template_path = args.template
+else:
+    env = Environment(loader=FileSystemLoader('./templates/'))
+    print('Available templates:')
+    for template in env.list_templates():
+        print(template)
+    template_path = input('Enter the name of the template file: ')
+
+
+# Load the Jinja2 template
+template = env.get_template(template_path)
 
 # Render the template with the config and write it to linkjinja.html
 output = template.render(config)
 
-with open('linkjinja.html', 'w') as f:
-    f.write(output)
-
-print('Done! Saved as linkjinja.html')
+if args.output:
+    with open(args.output, 'w') as f:
+        f.write(output)
+        print(f'Output written to {args.output}')
+else:
+    print(output)
